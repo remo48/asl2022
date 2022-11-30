50eedbb8ad6978f2fe1bc59383e0a7fa28464868
remok <remok@student.ethz.ch>
2022-11-30 16:20:38 +0100

    fixed backup

--- shared/ansible/roles/backup/files/backup-ca.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dir=/home/backup/ca/certs/`date '+%Y%m%d%H%M%S'`
-mkdir $dir
-rsync -a ca@ca:/home/ca/ca/certs/ $dir
--- shared/ansible/roles/backup/files/backup-web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dir=/home/backup/web/logs/`date '+%Y%m%d%H%M%S'`
-mkdir $dir
-rsync -a web@web:/home/web/web-server/web-server.log $dir
